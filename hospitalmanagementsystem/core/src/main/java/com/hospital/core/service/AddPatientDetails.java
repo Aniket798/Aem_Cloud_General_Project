a38a8a7c2240c174ddc95aa5ef0e0150e9ab818c
anike <aniketpandey798@gmail.com>
2023-11-06 11:27:24 +0530

    Revert "inital code commit"
    This reverts commit 8477034f7303b39cd798185b8d9ef3e4f949c364.

--- hospitalmanagementsystem/core/src/main/java/com/hospital/core/service/AddPatientDetails.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.core.service;
-
-public interface AddPatientDetails {
-
-    public String addPatientDetails(String patientName, String patientId, String assignedRoomNumber);
-
-    }
